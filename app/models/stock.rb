616cc6842e12881042c550a7d2473c54a6567f6a
hamza <hafizhamzasaleem4@gmail.com>
2020-11-17 18:10:44 +0500

    relation build

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,9 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence:true
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
         publishable_token: 'Tpk_b51e69522bab4742b8413a76618c534e',
@@ -9,7 +13,11 @@ class Stock < ApplicationRecord
     new(ticker:ticker_symbol,name:client.company(ticker_symbol).company_name,last_price:client.price(ticker_symbol))
     rescue => exception
       return nil
-      end
+    end
+  end
+
+  def self.check_ticker_in_db(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
 
 end
